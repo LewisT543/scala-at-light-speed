2f7dddc3ea92e5ccfc7b9ecd00ee08e4610a8f72
Lewis <lewisthornton@btinternet.com>
2023-05-29 13:10:11 +0100

    Initial commit

--- src/main/scala/com/rockthejvm/ObjectOrientation.scala
+++ src/main/scala/com/rockthejvm/ObjectOrientation.scala
@@ -2,114 +2,104 @@ package com.rockthejvm
 
 object ObjectOrientation extends App {
 
-  // java equivalent: public static void main(String[] args)
-
   // class and instance
   class Animal {
-    // define fields
     val age: Int = 0
-    // define methods
-    def eat() = println("I'm eating")
+    def eat(): Unit = println("I'm eating")
   }
-
   val anAnimal = new Animal
 
   // inheritance
   class Dog(val name: String) extends Animal // constructor definition
-  val aDog = new Dog("Lassie")
+  val newDog = new Dog("Lassie")
 
-  // constructor arguments are NOT fields: need to put a val before the constructor argument
-  aDog.name
+  // constructor arguments are not fields: we need to add val to the constructor definition to make it a field
+  newDog.name
 
   // subtype polymorphism
-  val aDeclaredAnimal: Animal = new Dog("Hachi")
-  aDeclaredAnimal.eat() // the most derived method will be called at runtime
+  val aDeclaredAnimal: Animal =  new Dog("Roger")
+  aDeclaredAnimal.eat() // the most derived(specific) method will be called at runtime
 
   // abstract class
   abstract class WalkingAnimal {
-    val hasLegs = true // by default public, can restrict by adding protected or private
+    val hasLegs = true // by default all fields are public, restrict using private / protected
     def walk(): Unit
+    private val isPrivate = true // viewable in the class only
+    protected val isProtected = true // usable in the class, package and subclasses
   }
 
-  // "interface" = ultimate abstract type
+  // interface - the ultimate abstract type
   trait Carnivore {
     def eat(animal: Animal): Unit
   }
 
   trait Philosopher {
-    def ?!(thought: String): Unit // valid method name
+    def ?!(thoughts: String): Unit // valid method name, allowing for interesting natural language looking code
   }
 
   // single-class inheritance, multi-trait "mixing"
   class Crocodile extends Animal with Carnivore with Philosopher {
-    override def eat(animal: Animal): Unit = println("I am eating you, animal!")
-
-    override def ?!(thought: String): Unit = println(s"I was thinking: $thought")
+    override def eat(animal: Animal): Unit = println(s"I am eating you, $animal") // allows for specific implementation
+    override def eat(): Unit = super.eat() // allows for usage of Superclass implementation
+    override def ?!(thoughts: String): Unit = println(s"I was thinking: $thoughts")
   }
 
   val aCroc = new Crocodile
-  aCroc.eat(aDog)
-  aCroc eat aDog // infix notation = object method argument, only available for methods with ONE argument
-  aCroc ?! "What if we could fly?"
+  aCroc.eat(newDog)
+  aCroc eat newDog // infix notation = object method argument - only available for methods with ONE argument
 
-  // operators in Scala are actually methods
-  val basicMath = 1 + 2
-  val anotherBasicMath = 1.+(2) // equivalent
+  // operators are methods in Scala
+  val basicMaths = 1 + 2
+  val anotherBasicmath = 1.+(2)
 
   // anonymous classes
   val dinosaur = new Carnivore {
-    override def eat(animal: Animal): Unit = println("I am a dinosaur so I can eat pretty much anything")
+    override def eat(animal: Animal): Unit = println("I am a dinosaur, I can eat anything")
   }
 
   /*
-    What you tell the compiler:
-
-    class Carnivore_Anonymous_35728 extends Carnivore {
-      override def eat(animal: Animal): Unit = println("I am a dinosaur so I can eat pretty much anything")
-    }
-
-    val dinosaur = new Carnivore_Anonymous_35728
-   */
+  * class Carnivore_Anonymous_32576 extends Carnivore {
+  *   override def eat(animal: Animal): Unit = println("I am a dinosaur, I can eat anything" )
+  * }
+  * val dinosaur = new Carnivore_Anonymous_32576
+  */
 
   // singleton object
   object MySingleton { // the only instance of the MySingleton type
-    val mySpecialValue = 53278
-    def mySpecialMethod(): Int = 5327
+    val mySpecialValue = 54321
+    def mySpecialMethod(): Int = 9876
     def apply(x: Int): Int = x + 1
   }
-
   MySingleton.mySpecialMethod()
-  MySingleton.apply(65)
-  MySingleton(65) // equivalent to MySingleton.apply(65)
+  MySingleton.apply(65) // these two lines are the same
+  MySingleton(65)       // these two lines are the same
 
-  object Animal { // companions - companion object
+  object Animal { // a class and an object with the same name are called companions - companion object
     // companions can access each other's private fields/methods
     // singleton Animal and instances of Animal are different things
-    val canLiveIndefinitely = false
+    val canLiveIndefinately = false
   }
 
-  val animalsCanLiveForever = Animal.canLiveIndefinitely // "static" fields/methods
+  val animalsCanLiveForever = Animal.canLiveIndefinately // accessed in same way as static variables in other languages
 
-  /*
-   case classes = lightweight data structures with some boilerplate
-   - sensible equals and hash code
-   - serialization
-   - companion with apply
-   - pattern matching
-  */
+  /* case classes = lightweight data structures with some boilerplate
+  *   - sensible equals and hashcode
+  *   - serialization
+  *   - companion with apply
+  * */
   case class Person(name: String, age: Int)
-  // may be constructed without new
-  val bob = Person("Bob", 54) // Person.apply("Bob", 54)
+  val bob = new Person("Bob", 24)
+  val alsoBob = Person("AlsoBob", 25) // => Person.apply("AlsoBob", 25)
 
   // exceptions
   try {
     // code that can throw
     val x: String = null
     x.length
-  } catch { // in Java: catch(Exception e) {...}
-    case e: Exception => "some faulty error message"
+  } catch {
+    case e: Exception => "Some faulty error message"
   } finally {
-    // execute some code no matter what
+    // execute some code no matter what (like closing connection to a db)
   }
 
   // generics
@@ -118,23 +108,20 @@ object ObjectOrientation extends App {
     def tail: MyList[T]
   }
 
-  // using a generic with a concrete type
   val aList: List[Int] = List(1,2,3) // List.apply(1,2,3)
-  val first = aList.head // int
-  val rest = aList.tail
-  val aStringList = List("hello", "Scala")
+  val first = aList.head // the generic provides the type for this val: Int
+  val rest = aList.tail // and this one
+
+  val aStringList = List("hello", "scala") // the whole point of generics is to re-use the same code for different types
   val firstString = aStringList.head // string
 
-  // Point #1: in Scala we usually operate with IMMUTABLE values/objects
-  // Any modification to an object must return ANOTHER object
+  // Point #1: in Scala we usually operate with Immutable values/objects
+  val reversedListt = aList.reverse // returns a new object
   /*
     Benefits:
-    1) works miracles in multithreaded/distributed env
-    2) helps making sense of the code ("reasoning about")
+    1). Works miracles in multithreaded environments
+    2). helps make sense of the code ("reasoning about the code")
    */
-  val reversedList = aList.reverse // returns a NEW list
 
-  // Point #2: Scala is closest to the OO ideal
-
-
+  // Point #2: Scala is closest to the OO ideal, and is actually a very good OO language
 }
